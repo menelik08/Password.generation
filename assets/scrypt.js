c4ffa57b038f84c4d62944cb90cd5aa4e26bf1bf
Yosef Teshome <menelik08@gmail.com>
2022-10-27 19:03:47 -0400

    CSS sheet

--- assets/scrypt.js
+++ assets/scrypt.js
@@ -1,14 +1,17 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var chars = "01234567890abcdefghijklmnopqrstuvwxyz!@#$%^&*()"; 
+var passwordLenghit = 12
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+   
   passwordText.value = password;
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+writePassword();
