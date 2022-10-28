dc4817c76cb78fcd7f6a5b502e1d7efdcec49c1a
Yosef Teshome <menelik08@gmail.com>
2022-10-27 23:07:09 -0400

    completed java

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Password.generation
+
+## Description
+
+Project was to have a website that will help in creating a random password. 
+The requirements were to have Upper and Lowercase letters and also include numbers and special characters.
+The webiste should prompt the user to select the items and the desired lenght, which was between 8 and 128 characters. 
+Had to do research about how to use Math functions and using the add event listner. 
--- assets/scrypt.js
+++ assets/scrypt.js
@@ -1,17 +1,67 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var chars = "01234567890abcdefghijklmnopqrstuvwxyz!@#$%^&*()"; 
-var passwordLenghit = 12
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-   
   passwordText.value = password;
+}
+// Add event listener to generate button
+generateBtn.addEventListener("OnClick", writePassword);
+
+var upper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var lower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var num = ["1","2","3","4","5","6","7","8","9","0"];
+var spec = ["!","@","#","$","%","^","&","*","(",")"]
+var result = '';
+
+// Added Functions to create parameters for password setup
+
+function generatePassword() {
+    var uppercase = window.confirm("Include Uppercase Letters?");
+    var lowercase = window.confirm("Include Lowercase Letters?");
+    var numeric = window.confirm("Include Numbers?");
+    var special = window.confirm("Include Special Characters?");
+    var length = prompt("Enter the Number of Characters Desired. Must be Between 8-128");
+    var blank = [];
+
+    if (length<8){
+        alert("Password Can Not be Longer Than 128")
+        return 
+    }
+    else if (length>128){
+        alert("Password Must Contain At Least 8 Characters")
+        return
+    }
+    if (uppercase){
+        blank = blank.concat(upper);
+    } 
+    if (lowercase){
+        blank = blank.concat(lower);
+    } 
+    if (numeric){
+        blank = blank.concat(num);
+    } 
+    if (special){
+        blank = blank.concat(spec);
+    } 
+    if (blank.length<1) {
+        alert("No Characters to Choose From!")
+        return
+    } 
+    function Create(){
+        var result = [];
+        function number(max){
+            return Math.floor(Math.random()*max)
+        }
+        for (var i=0; i<length; i++){
+            result = result.concat(blank[number(blank.length)])
+        }
+        return final = result.join("");
+    }
+    console.log(blank)
+    console.log()
+    return Create();
 
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-writePassword();
